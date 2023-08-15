270d589910192a8f13f8480970d6556b1ed1d4ed
booosty <waf77@arastudent.ac.nz>
2023-08-15 15:57:04 +1200

    Add command line argument check and .gitignore

--- src/main.py
+++ src/main.py
@@ -1,6 +1,9 @@
+import sys
 from Commands import Commands
 
 
 if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        print(f"You provided {len(sys.argv) - 1} command line arguments.")
     commands = Commands()
     commands.cmdloop()
