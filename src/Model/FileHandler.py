553c22a3089bd7e7d5e5ec61f53eeaeac1d4882f
booosty <waf77@arastudent.ac.nz>
2023-08-24 19:50:54 +1200

    Create save directory

--- src/Model/FileHandler.py
+++ src/Model/FileHandler.py
@@ -7,22 +7,22 @@ from Model.ImageHandler import ImageHandler
 
 class FileHandler:
     def __init__(self):
-        self.root_dir = Path(__file__).parent.parent / "Data"
+        self.root_dir = Path(__file__).parent.parent
 
     def load_data_from_json(self, filename):
-        file = open(str(self.root_dir) + "\\" + filename + ".json")
+        file = open(str(self.root_dir / "Data") + "\\" + filename + ".json")
         data = json.load(file)
         file.close()
         return data
 
     # Junho
     def save_game_with_pickle(self, game, filename):
-        with open(str(self.root_dir) + "\\" + filename + ".pkl", "wb") as file:
+        with open(str(self.root_dir / "Saves") + "\\" + filename + ".pkl", "wb") as file:
             pickle.dump(game, file)
 
     # Junho
     def load_game_with_pickle(self, filename):
-        with open(str(self.root_dir) + "\\" + filename + ".pkl", "rb") as file:
+        with open(str(self.root_dir / "Saves") + "\\" + filename + ".pkl", "rb") as file:
             game = pickle.load(file)
 
         # Restore the image_handler attribute
