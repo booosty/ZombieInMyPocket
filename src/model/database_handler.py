3fac349a844f2798a84a43ec9e8a359e398f39c9
booosty <waf77@arastudent.ac.nz>
2023-11-21 22:33:26 +1300

    Refactor 1: Create Concrete Product - SQLite

--- src/model/database_handler.py
+++ src/model/database_handler.py
@@ -21,6 +21,7 @@ class IDatabaseHandler(ABC):
     """
     DatabaseHandler Interface
     """
+
     def save(self, game):
         pass
 
@@ -32,6 +33,7 @@ class SQLiteConnectionFactory(DatabaseConnectionFactory):
     """
     Concrete Factory for SQLite
     """
+
     def create_connection(self, path):
         connection = None
         try:
@@ -49,6 +51,50 @@ class SQLiteConnectionFactory(DatabaseConnectionFactory):
 
     def create_cursor(self, db_connection):
         return db_connection.cursor()
+
+
+class SQLiteHandler(IDatabaseHandler):
+    """
+    Concrete Product - SQLite
+    """
+
+    def __init__(self, connection_factory):
+        self.connection_factory = connection_factory
+
+    def save(self, game):
+        con = self.connection_factory.create_connection("saves/game_save.db")
+        cursor = self.connection_factory.create_cursor(con)
+        data = pickle.dumps(game, pickle.HIGHEST_PROTOCOL)
+
+        try:
+            cursor.execute("""DROP TABLE IF EXISTS game""")
+            cursor.execute("""CREATE TABLE game(id, data)""")
+            cursor.execute("""INSERT INTO game VALUES (?, ?)""", (1, Binary(data)))
+        except Error as err:
+            print(f'Sql error: {" ".join(err.args)}')
+            print(f'Exception class is: {err.__class__}')
+        finally:
+            con.commit()
+            con.close()
+
+    def load(self):
+        data = None
+        con = self.connection_factory.create_connection("saves/game_save.db")
+        cursor = self.connection_factory.create_cursor(con)
+
+        try:
+            cursor.execute("SELECT data FROM game")
+            rows = cursor.fetchall()
+
+            for r in rows[0]:
+                data = pickle.loads(r)
+
+        except Error as err:
+            print(f'Sql error: {" ".join(err.args)}')
+            print(f'Exception class is: {err.__class__}')
+        finally:
+            con.close()
+            return data
 
 
 # William
